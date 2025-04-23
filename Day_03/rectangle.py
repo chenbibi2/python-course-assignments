832221a4325062609af599aebad0e5795c623289
chenbibi2 <bibichen2@gmail.com>
2025-04-23 11:51:09 +0300

    Update rectangle.py

--- Day_03/rectangle.py
+++ Day_03/rectangle.py
@@ -1,6 +1,6 @@
 import math
-height = float(sys.argv("Enter the height of the rectangle: "))
-width = float(sys.argv("Enter the width of the rectangle: "))
+height = float(sys.argv[1])
+width = float(sys.argv[2])
 area = height * width
 perimeter = 2 * (height + width)
 print(f"The area of the rectangle is: {area:}")
