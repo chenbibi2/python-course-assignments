3d7bdc5eaacd5dcbd50efb01ad89f5f777f51346
chenbibi2 <bibichen2@gmail.com>
2025-05-22 16:11:00 +0300

    Update number_guessing_game_0.py

--- Day_04/number_guessing_game_0.py
+++ Day_04/number_guessing_game_0.py
@@ -1,1 +1,10 @@
-
+import random
+number= random.randint (1,20)
+  print ("I am thinking of a number between 1 and 20, can you guess what it is?" )
+  guess = int(input("your guess is:"))
+  if number < guess:
+    print ("The number you entered is lower than what I chose")
+  elif number > guess:
+    print ("The number you entered is higher than what I chose")
+    else :
+    print("Congratulations! You guessed the right number!")
