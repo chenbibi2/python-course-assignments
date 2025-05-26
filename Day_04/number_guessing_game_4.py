b962adb67832fb4d62505e7f187efa61fa064efa
chenbibi2 <bibichen2@gmail.com>
2025-05-26 11:40:53 +0300

    Update number_guessing_game_4.py

--- Day_04/number_guessing_game_4.py
+++ Day_04/number_guessing_game_4.py
@@ -1,22 +1,26 @@
 import random
-
 number = random.randint(1, 20)
 print("I am thinking of a number between 1 and 20. Can you guess what it is?")
 print("Type 'x' to quit the game.")
-print("Type 's' to know the number.")
-print("Type 'd' to enter debug mode")
-guess =0
+print("Type 's' to reveal the number.")
+print("Type 'd' to enter debug mode.")
+debug = False  
 while True:
+    if debug:
+        print(f"(Debug Mode) The number is {number}")
     guess = input("Your guess is: ")
-    
     if guess == "x":
         print("You quit the game.")
         break
     elif guess == "s":
         print(f"The number is: {number}")
         break
-    elif guess== "d": 
-
+    elif guess == "d":
+        debug = not debug
+        if debug:
+            print("Debug mode is now ON.")
+        else:
+            print("Debug mode is now OFF.")
     elif guess.isdigit():
         guess = int(guess)
         if guess < number:
@@ -26,3 +30,5 @@ while True:
         else:
             print("Congratulations! You guessed the right number!")
             break
+
+
