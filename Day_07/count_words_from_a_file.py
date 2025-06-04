233d2e26a14ac7b3548c142078e4df18d920e43a
chenbibi2 <bibichen2@gmail.com>
2025-06-04 14:36:00 +0300

    Update count_words_from_a_file.py

--- Day_07/count_words_from_a_file.py
+++ Day_07/count_words_from_a_file.py
@@ -1,1 +1,11 @@
+import sys
+from collections import Counter
+filename = sys.argv[1]
+with open(filename, 'r') as file:
+   content = file.read()
+words = content.split()
+word_counts = Counter(words)
+for word, count in sorted(word_counts.items()):
+    print(f"{word}: {count}")
+
 
