2f48b5f5f26d5896f8d442e6ce88f665aeee4d6f
chenbibi2 <bibichen2@gmail.com>
2025-06-12 15:10:38 +0300

    Update script_for_the_calculation.py

--- Day_08/script_for_the_calculation.py
+++ Day_08/script_for_the_calculation.py
@@ -1,9 +1,10 @@
-from circle_calculations import choose_radius,calculate_area, calculate_circumference,Print_results
+from circle_calculations import choose_radius, calculate_area, calculate_circumference, print_results
+
 def main():
-    radius = float(input("Enter the radius of the circle: "))
+    radius = choose_radius()
     area = calculate_area(radius)
     circumference = calculate_circumference(radius)
-    print(f"The area of the circle is: {area:.2f}")
-    print(f"The circumference of the circle is: {circumference:.2f}")
+    print_results(area, circumference)
+
 if __name__ == "__main__":
     main()
