403c50a78bfd85133c259f94cd0d9cc7955bdca2
chenbibi2 <bibichen2@gmail.com>
2025-06-18 16:33:24 +0300

    Update circle_calculations.py

--- Day_09/circle_calculations.py
+++ Day_09/circle_calculations.py
@@ -1,7 +1,4 @@
 import math
-
-def choose_radius (radius):
-    radius = float(input("Enter the radius of the circle: "))
     
 def  calculate_area (radius):
     """
@@ -22,6 +19,3 @@ def calculate_circumference (radius):
     12.56637061
     """
     return 2 * math.pi * radius
-def Print_results (area,circumference):
-    print(f"The area of the circle is: {area:.2f}")
-    print(f"The circumference of the circle is: {circumference:.2f}")
