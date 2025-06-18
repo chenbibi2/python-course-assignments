d5e00ffa8ca3fc1f9cbc9425e22aa487ed6df0d7
chenbibi2 <bibichen2@gmail.com>
2025-06-18 16:54:08 +0300

    Update circle_calculation_test.py

--- Day_09/circle_calculation_test.py
+++ Day_09/circle_calculation_test.py
@@ -1,1 +1,9 @@
+import unittest
+import math
 
+class TestMath(unittest.TestCase):
+    def test_math(self):
+        self.assertEqual(mymath.add(2, 2), 4)
+
+    def test_more_math(self):
+        self.assertEqual(mymath.add(3, 3), 6)
