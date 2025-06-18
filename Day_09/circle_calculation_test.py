f1baf71fcb7db432bcc78459e0c1be42c6c2c095
chenbibi2 <bibichen2@gmail.com>
2025-06-18 17:10:48 +0300

    Update circle_calculation_test.py

--- Day_09/circle_calculation_test.py
+++ Day_09/circle_calculation_test.py
@@ -4,7 +4,10 @@ import math
 
 class TestMath(unittest.TestCase):
     def test_area(self):
-        self.assertEqual(circle_calculations.calculate_area(2), 12.56637061)
+        self.assertAlmostEqual(circle_calculations.calculate_area(2), 12.56637061)
 
-    def test_more_math(self):
-        self.assertEqual(circle_calculations.calculate_circumference(1),6.283185307)
+    def test_circumference(self):
+        self.assertAlmostEqual(circle_calculations.calculate_circumference(1),6.283185307)
+
+if __name__ == "__main__":
+    unittest.main()
