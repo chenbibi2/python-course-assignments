0ab206c15a09a694ecae1b33fe5efa9c2c4797bd
chenbibi2 <bibichen2@gmail.com>
2025-06-18 16:28:08 +0300

    Update script_for_the_calculation.py

--- Day_09/script_for_the_calculation.py
+++ Day_09/script_for_the_calculation.py
@@ -1,1 +1,10 @@
+from circle_calculations import choose_radius, calculate_area, calculate_circumference, print_results
 
+def main():
+    radius = choose_radius()
+    area = calculate_area(radius)
+    circumference = calculate_circumference(radius)
+    print_results(area, circumference)
+
+if __name__ == "__main__":
+    main()
